d3bec2f31f7b6adeee74a940643e20566dfd2d83
Abdul Khaliq Abdul-Azeez <abdulkhailqabdulazeez@gmail.com>
2021-07-19 18:23:28 +0000

    fix

--- model.py
+++ model.py
@@ -8,5 +8,5 @@ def getImageUrlFrom(gif_name):
     giphy_request_link = "https://api.giphy.com/v1/gifs/search?api_key=PIUIXIN36OgwPazg4C4fzP54lzLZmSrK&q="+rand+"&limit=25&offset=0&rating=g&lang=en"
     respone = requests.get(giphy_request_link).json()
     random_number= random.randrange(0,25)
-    return respone["data"][random_number]["images"]["fixed_height"]["url"]
+    return respone["data"][random_number]["images"]["downsized_large"]["url"]
    
